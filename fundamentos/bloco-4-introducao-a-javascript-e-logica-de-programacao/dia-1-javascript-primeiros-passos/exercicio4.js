8b9d4882801784587b32156388b9c53813af578e
LuizOtavio-dev <luiz.otavio273@gmail.com>
2022-07-04 22:24:00 -0300

    adiciona um programa que retorna se o valor e positivo, negativo ou zero

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio4.js
@@ -0,0 +1,12 @@
+const number = 0;
+let positiveOrNegative;
+
+if (number > 0) {
+  positiveOrNegative = "positive";
+} else if (number < 0) {
+  positiveOrNegative = "negative";
+} else {
+  positiveOrNegative = "zero";
+}
+
+console.log(positiveOrNegative);
