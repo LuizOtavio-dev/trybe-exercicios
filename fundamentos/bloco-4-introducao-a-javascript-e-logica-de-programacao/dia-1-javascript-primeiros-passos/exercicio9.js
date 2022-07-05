6610181d0b8975765623f0119d76f87b78223193
LuizOtavio-dev <luiz.otavio273@gmail.com>
2022-07-05 20:53:07 -0300

    adiciona um programa que retorna true se um dos tres numeros for impar

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio9.js
@@ -0,0 +1,10 @@
+const a = Math.floor(Math.random() * 100) + 1;
+const b = Math.floor(Math.random() * 100) + 1;
+const c = Math.floor(Math.random() * 100) + 1;
+let trueFalse;
+
+if (a % 2 !== 0 || b % 2 !== 0 || c % 2 !== 0) {
+  trueFalse = true;
+} else {
+  trueFalse = false;
+}
