c97784a67a569ae65a7d5d27f506f2627fd18e9d
LuizOtavio-dev <luiz.otavio273@gmail.com>
2022-07-08 16:11:56 -0300

    exercicio 3 dois algaritmos que retorna a maiior e a menor palavra

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/tempCodeRunnerFile.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/tempCodeRunnerFile.js
@@ -1,29 +1,3 @@
-// exercício 1
-
-const factorial = 10;
-let resultado = factorial;
-
-for (let index = factorial - 1; index > 0; index -= 1) {
-  resultado *= index;
-}
-
-console.log(resultado);
-
-// exercício 2
-
-let word = 'luiz';
-let reverseWord = ''
-let otherWay = '';
-
-for (let index = word.length - 1; index >= 0; index -= 1) {
-  reverseWord = reverseWord.concat(word[index]);
-  otherWay = otherWay + word[index]
-};
-
-console.log(reverseWord);
-console.log(otherWay);
-
-// exercício 3
 
 let array = ['java', 'javascript', 'python', 'html', 'css'];
 let biggestWord = array[0];
