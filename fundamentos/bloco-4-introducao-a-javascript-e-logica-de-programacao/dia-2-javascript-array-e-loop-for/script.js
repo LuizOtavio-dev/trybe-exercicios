c43aa275f847e005e09b15ad3556581917b39c15
LuizOtavio-dev <luiz.otavio273@gmail.com>
2022-07-08 00:03:22 -0300

    exercicio 5 imprime o maior numero contido no array

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -29,3 +29,14 @@ if (media > 20) {
 } else {
   console.log("valor menor ou igual a 20");
 }
+
+// exercicio 5
+
+let higherNumber = 0;
+for (let index = 0; index < numbers.length; index += 1) {
+  if (numbers[index] > higherNumber) {
+    higherNumber = numbers[index]
+  }  
+}
+
+console.log(higherNumber);
