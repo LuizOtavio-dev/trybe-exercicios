708d3919a07ce374bf3fd046a58790e2f48108e8
LuizOtavio-dev <luiz.otavio273@gmail.com>
2022-07-07 21:29:01 -0300

    exercicio 3 calcula a media aritmetica dos valores contidos no array

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -1,12 +1,23 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let total = 0;
+
+// exercício 1
 
 for (let index = 0; index < numbers.length; index += 1) {
   console.log(numbers[index]);
 };
+
+// exercício 2
+
+let total = 0;
 
 for (let index = 0; index < numbers.length; index += 1) {
   total += numbers[index];
 };
 
 console.log(total);
+
+// exercício 3
+
+let media = total / numbers.length;
+
+console.log(media);
