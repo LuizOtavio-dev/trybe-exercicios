b3aa488138d11a059cb87a8e3dbce080c7e69ae5
LuizOtavio-dev <luiz.otavio273@gmail.com>
2022-07-09 14:53:48 -0300

    exercicio bonus imprime um triangulo retangulo com asteriscos

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -75,3 +75,12 @@ const asterisk = '*';
 for (let index = 0; index <= n; index += 1) {
   console.log(asterisk.padStart(n, asterisk));
 };
+
+// exercício 2
+
+let lineAsterisk = '';
+
+for (let index = 0; index <= n; index += 1) {
+  lineAsterisk += asterisk;
+  console.log(lineAsterisk);
+};
--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/tempCodeRunnerFile.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/tempCodeRunnerFile.js
@@ -1,7 +1,15 @@
-
-const n = 10;
+const n = 5;
 const asterisk = '*';
 
 for (let index = 0; index <= n; index += 1) {
   console.log(asterisk.padStart(n, asterisk));
+};
+
+// exercício 2
+
+let lineAsterisk = '';
+
+for (let index = 0; index <= n; index += 1) {
+  lineAsterisk += asterisk;
+  console.log(lineAsterisk);
 };
