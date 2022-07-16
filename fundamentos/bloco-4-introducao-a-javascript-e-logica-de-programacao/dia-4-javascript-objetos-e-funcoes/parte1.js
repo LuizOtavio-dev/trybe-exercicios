9675d088ad40dc8d9bc3ff182b1eb54f0cabceee
LuizOtavio-dev <luiz.otavio273@gmail.com>
2022-07-16 01:05:15 -0300

    exercicio 5 - parte 1 - cria um novo objeto com a mesma estrutura do primeiro e imprime os valores de cada objeto juntos

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/parte1.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/parte1.js
@@ -4,6 +4,13 @@ let info = {
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
 };
 
+let infoTioPatinhas = {
+  personagem: "Tio Patinhas",
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: "O último MacPatinhas",
+  recorrente: "Sim",
+};
+
 console.log('Bem-vinda, ' + info.personagem);
 
 info.recorrente = 'Sim';
@@ -15,4 +22,10 @@ for (const key in info) {
 
 for (const key in info) {
   console.log(info[key]);
+}
+
+for (const key in info) {
+  if (info[key] !== infoTioPatinhas[key]) {
+    console.log(info[key] + ' e ' + infoTioPatinhas[key]);
+  } else {console.log('Ambos recorrentes')}
 }
