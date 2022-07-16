ee91877ca8f5392c3bac63811d07cea5c1513883
LuizOtavio-dev <luiz.otavio273@gmail.com>
2022-07-16 01:20:07 -0300

    exercicio 6 - parte 1 - cria um novo objeto e acessa as chaves nome, sobrenome e titulo e imprime uma frase

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/parte1.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/parte1.js
@@ -29,3 +29,18 @@ for (const key in info) {
     console.log(info[key] + ' e ' + infoTioPatinhas[key]);
   } else {console.log('Ambos recorrentes')}
 }
+
+let leitor = {
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: 'O Pior Dia de Todos',
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas',
+    },
+  ],
+};
+
+console.log("O livro favorito de " + leitor.nome + " " + leitor.sobrenome + " se chama "  + "'" + leitor.livrosFavoritos[0].titulo + "'");
--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/tempCodeRunnerFile.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/tempCodeRunnerFile.js
@@ -1,10 +1,15 @@
-let info = {
-  personagem: 'Margarida',
-  origem: 'Pato Donald',
-  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+
+let leitor = {
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: 'O Pior Dia de Todos',
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas',
+    },
+  ],
 };
 
-console.log('Bem-vinda, ' + info.personagem);
-
-info.recorrente = 'Sim';
-console.log(info);
+console.log("O livro favorito de " + leitor.nome + " " + leitor.sobrenome + " se chama "  + "'" + leitor.livrosFavoritos[0].titulo + "'");
