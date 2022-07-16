1d77959de301e8f6f17cd2c2f27d77d65be4716b
LuizOtavio-dev <luiz.otavio273@gmail.com>
2022-07-16 01:25:17 -0300

    exercicio 7 - parte 1 - adiciona um novo livro favorito

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/tempCodeRunnerFile.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/tempCodeRunnerFile.js
@@ -13,3 +13,11 @@ let leitor = {
 };
 
 console.log("O livro favorito de " + leitor.nome + " " + leitor.sobrenome + " se chama "  + "'" + leitor.livrosFavoritos[0].titulo + "'");
+
+leitor.livrosFavoritos.push({
+  titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+  autor: 'JK Rowling',
+  editora: 'Rocco',
+});
+
+console.log(leitor);
