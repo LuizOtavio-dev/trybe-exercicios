a6ce34842e115fa9777c38c66fa09ea6191589be
LuizOtavio-dev <luiz.otavio273@gmail.com>
2022-07-16 22:30:51 -0300

    exercicio 5 - parte 2 - cria uma funcao que recebe um array de inteiros e retorna o que mais se repete

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/parte2.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/parte2.js
@@ -65,3 +65,30 @@ function biggestName(array) {
 }
 
 console.log(biggestName(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']));
+
+// 5
+
+function numberThatRepeatsTheMost(array) {
+  let countNumbers = 0;
+  let highestRepetition = 0;
+  let number = array[0];
+
+  for (let index = 0; index < array.length; index += 1) {
+    for (let counter = 0; counter < array.length; counter += 1) {
+      if (array[index] === array[counter]) {
+        countNumbers += 1;
+      }
+    }
+
+    if (countNumbers > highestRepetition) {
+      highestRepetition = countNumbers;
+      number = array[index];
+    }
+    
+    countNumbers = 0;
+  }
+
+  return number;
+}
+
+console.log(numberThatRepeatsTheMost([2, 3, 2, 5, 8, 2, 3]));
--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/tempCodeRunnerFile.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/tempCodeRunnerFile.js
@@ -1,13 +1,24 @@
-function biggestName(array) {
-  let compareNames = array[0]
+function numberThatRepeatsTheMost(array) {
+  let countNumbers = 0;
+  let highestRepetition = 0;
+  let number = array[0];
 
-  for (const iterator of array) {
-    if (iterator.length > compareNames.length) {
-      compareNames = iterator
+  for (let index = 0; index < array.length; index += 1) {
+    for (let counter = 0; counter < array.length; counter += 1) {
+      if (array[index] === array[counter]) {
+        countNumbers += 1;
+      }
     }
+
+    if (countNumbers > highestRepetition) {
+      highestRepetition = countNumbers;
+      number = array[index];
+    }
+    
+    countNumbers = 0;
   }
 
-  return compareNames;  
+  return number;
 }
 
-console.log(biggestName(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']));
+console.log(numberThatRepeatsTheMost([2, 3, 2, 5, 8, 2, 3]));
