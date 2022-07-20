7e8d41014c3c43aa9246253a8295390a0307c6f1
LuizOtavio-dev <luiz.otavio273@gmail.com>
2022-07-19 23:01:23 -0300

    exercicio 4 - bonus - cria uma funcao que acessa chaves do ultimo morador

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/bonus.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/bonus.js
@@ -99,3 +99,46 @@ function objectOfFruits(array) {
 }
 
 objectOfFruits(basket);
+
+// 4
+
+let moradores = {
+  blocoUm: [
+    {
+      nome: 'Luiza',
+      sobrenome: 'Guimarães',
+      andar: 10,
+      apartamento: 1005,
+    },
+    {
+      nome: 'William',
+      sobrenome: 'Albuquerque',
+      andar: 5,
+      apartamento: 502,
+    },
+  ],
+  blocoDois: [
+    {
+      nome: 'Murilo',
+      sobrenome: 'Ferraz',
+      andar: 8,
+      apartamento: 804,
+    },
+    {
+      nome: 'Zoey',
+      sobrenome: 'Brooks',
+      andar: 1,
+      apartamento: 101,
+    },
+  ],
+};
+
+function lastResident(object) {
+  const name = object.blocoDois[object.blocoDois.length - 1].nome;
+  const lastName = object.blocoDois[object.blocoDois.length - 1].sobrenome;
+  const flat = object.blocoDois[object.blocoDois.length - 1].andar;
+  const apartment = object.blocoDois[object.blocoDois.length - 1].apartamento;
+  console.log('O morador do bloco 2 de nome ' + name + ' ' + lastName + ' mora no ' + flat + '° andar, apartamento ' + apartment);
+}
+
+lastResident(moradores);
