5c551ce606faf231fafe9d4e81ac7c28ee9356a3
LuizOtavio-dev <luiz.otavio273@gmail.com>
2022-07-23 22:54:08 -0300

    exercicio 4 - cria e executa uma funcao que corrije o texto da tag h1

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -19,6 +19,9 @@ section[0].style.backgroundColor = 'white';
 
 //  Crie e execute uma função que corrija o texto da tag <h1>.
 
+const h1 = document.getElementsByTagName('h1');
+h1[0].innerText = 'Exercício 5.1 - JavaScript';
+
 //  Crie e execute uma função que modifique todo o texto da tag <p> para maiúsculo.
 
 //  Crie e execute uma função que exiba o conteúdo de todas as tags <p> no console.
