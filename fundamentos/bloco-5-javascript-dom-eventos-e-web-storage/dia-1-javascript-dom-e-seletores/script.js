f41d36b7c78efa7c52db99b4fa6237ee6432c66a
LuizOtavio-dev <luiz.otavio273@gmail.com>
2022-07-23 22:41:04 -0300

    exercicio 1 - cria e executa uma funcao que modifica o texto na tag p

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -0,0 +1,18 @@
+// Aqui você vai modificar os elementos já existentes utilizando apenas as funções:
+//   - document.getElementById()
+//   - document.getElementsByClassName()
+//   - document.getElementsByTagName()
+//  Crie e execute uma função que mude o texto na tag `<p>-----</p>`, para uma descrição de como você se vê daqui a 2 anos. (Não gaste tempo pensando no texto e sim realizando o exercício)
+
+const p = document.getElementsByTagName('p');
+p[1].innerText = 'Daqui 2 anos me vejo trabalhando como Desenvolvedor';
+
+//  Crie e execute uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).
+
+//  Crie e execute uma função que mude a cor do quadrado vermelho para branco.
+
+//  Crie e execute uma função que corrija o texto da tag <h1>.
+
+//  Crie e execute uma função que modifique todo o texto da tag <p> para maiúsculo.
+
+//  Crie e execute uma função que exiba o conteúdo de todas as tags <p> no console.
