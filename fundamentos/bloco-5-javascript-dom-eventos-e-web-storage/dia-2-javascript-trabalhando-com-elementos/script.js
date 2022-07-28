26dbb8e01af7c0ce33d8928970b2c1215cba5667
LuizOtavio-dev <luiz.otavio273@gmail.com>
2022-07-28 14:10:03 -0300

    adiciona a tag main com uma classe como filho da tag body

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -2,6 +2,10 @@
 
 const body = document.body;
 const elementH1 = document.createElement('h1');
+const elementMain = document.createElement('main');
 
 elementH1.innerText = 'Exercício 5.2 - JavaScript DOM';
 body.appendChild(elementH1)
+
+elementMain.className = 'main-content';
+body.appendChild(elementMain);
