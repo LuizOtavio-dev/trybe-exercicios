f5723c717dd12dbe6c34c4e5d5bc99c9730f0d5c
LuizOtavio-dev <luiz.otavio273@gmail.com>
2022-07-28 14:14:28 -0300

    adiciona a tag section com uma classe como filho da tag main

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -3,9 +3,14 @@
 const body = document.body;
 const elementH1 = document.createElement('h1');
 const elementMain = document.createElement('main');
+const elementSection = document.createElement('section');
 
 elementH1.innerText = 'Exercício 5.2 - JavaScript DOM';
 body.appendChild(elementH1)
 
 elementMain.className = 'main-content';
 body.appendChild(elementMain);
+
+const main = document.getElementsByClassName('main-content')[0];
+elementSection.className = 'center-content';
+main.appendChild(elementSection);
