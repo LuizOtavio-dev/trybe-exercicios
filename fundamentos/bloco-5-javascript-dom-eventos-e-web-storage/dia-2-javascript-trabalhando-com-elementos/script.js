d5edfc68146aaa34a2aba0dc61ae1d9238d0daaf
LuizOtavio-dev <luiz.otavio273@gmail.com>
2022-07-28 14:20:41 -0300

    adiciona a tag p com um texto como filho da tag section

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -4,6 +4,7 @@ const body = document.body;
 const elementH1 = document.createElement('h1');
 const elementMain = document.createElement('main');
 const elementSection = document.createElement('section');
+const elementPara = document.createElement('p');
 
 elementH1.innerText = 'Exercício 5.2 - JavaScript DOM';
 body.appendChild(elementH1)
@@ -14,3 +15,7 @@ body.appendChild(elementMain);
 const main = document.getElementsByClassName('main-content')[0];
 elementSection.className = 'center-content';
 main.appendChild(elementSection);
+
+const section = document.getElementsByClassName('center-content')[0];
+elementPara.innerText = 'Parágrafo';
+section.appendChild(elementPara);
