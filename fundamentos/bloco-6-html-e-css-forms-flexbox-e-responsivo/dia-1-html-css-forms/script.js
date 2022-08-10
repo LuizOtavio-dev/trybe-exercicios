f6f2486126064081e9e0685215eb29148e7f7344
LuizOtavio-dev <luiz.otavio273@gmail.com>
2022-08-10 18:35:06 -0300

    exercicio 4 - cria um botao que limpa as informacoes

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -8,9 +8,23 @@ function createElementButton(text) {
 }
 
 createElementButton('Enviar');
+createElementButton('Limpar');
 
-const btnSubmit = document.querySelector('.btn');
+const btn = document.querySelectorAll('.btn');
+const input = document.querySelectorAll('input');
 
-btnSubmit.addEventListener('click', function(event) {
-  event.preventDefault();
-})
+for (let index = 0; index < btn.length; index += 1) {
+  if (btn[index].innerText === 'Enviar') {
+    btn[index].addEventListener('click', function(event) {
+      event.preventDefault();
+    })
+  }
+  if (btn[index].innerText === 'Limpar') {
+    btn[index].addEventListener('click', function() {
+      for (let index = 0; index < input.length; index += 1) {
+        input[index].value = '';
+      }
+    })
+  }
+}
+  
