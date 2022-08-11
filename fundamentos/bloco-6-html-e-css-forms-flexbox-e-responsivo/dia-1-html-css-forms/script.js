afa3c55af237148ab8480193a85c02cd563cb649
LuizOtavio-dev <luiz.otavio273@gmail.com>
2022-08-10 22:01:09 -0300

    exercicio 2 - bonus - faz a validacao dos campos com limites

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -12,22 +12,40 @@ createElementButton('Limpar');
 
 const btn = document.querySelectorAll('.btn');
 const input = document.querySelectorAll('input');
-const inputCheckbox = document.querySelector('#agree2')
+const inputCheckbox = document.querySelector('#agree2');
+const nameOfTheDev = document.querySelector('#name');
+const emailOfTheDev = document.querySelector('#email');
 
-for (let index = 0; index < btn.length; index += 1) {
-  if (btn[index].innerText === 'Enviar') {
-    btn[index].addEventListener('click', function(event) {
-      if (inputCheckbox.checked !== true) {
-        event.preventDefault();        
-      }
-    })
-  }
-  if (btn[index].innerText === 'Limpar') {
-    btn[index].addEventListener('click', function() {
-      for (let index = 0; index < input.length; index += 1) {
-        input[index].value = '';
-      }
-    })
+function sendButton() {
+  for (let index = 0; index < btn.length; index += 1) {
+    if (btn[index].innerText === 'Enviar') {
+      btn[index].addEventListener('click', function(event) {
+        if (inputCheckbox.checked !== true) {
+          event.preventDefault();        
+        }
+        if (nameOfTheDev.value.length > nameOfTheDev.max || emailOfTheDev.value.length > emailOfTheDev.max) {
+          alert('Dados Inválidos');
+        } else {
+          alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip')
+        }
+      })
+    }
   }
 }
+
+sendButton();
+
+function clearButton() {
+  for (let index = 0; index < btn.length; index += 1) {
+    if (btn[index].innerText === 'Limpar') {
+      btn[index].addEventListener('click', function() {
+        for (let index = 0; index < input.length; index += 1) {
+          input[index].value = '';
+        }
+      })
+    }
+  }
+}
+
+clearButton();
   
