c93a01c8bc6d974ce5c00e99141a7a2be269f0bf
LuizOtavio-dev <luiz.otavio273@gmail.com>
2022-08-15 23:26:59 -0300

    exercicio 2 - bonus - adiciona uma biblioteca js de validacoes ao formulario

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-2-bibliotecas-javascript-e-frameworks-css/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-2-bibliotecas-javascript-e-frameworks-css/script.js
@@ -6,6 +6,68 @@ const emailOfTheDev = document.querySelector('#email');
 const textareaAnswer = document.querySelector('#answer');
 const paraTextarea = document.querySelector('#ch-counter');
 
+const validation = new JustValidate('#form', {
+  errorFieldCssClass: 'is-invalid',
+  errorFieldStyle: {
+    border: '1px solid red',
+  },
+  errorLabelCssClass: 'is-label-invalid',
+  errorLabelStyle: {
+    color: 'red',
+    textDecoration: 'underlined',
+  },
+  focusInvalidField: true,
+  lockForm: true,
+  tooltip: {
+    position: 'top',
+  }
+});
+
+validation
+  .addField('#name', [
+    {
+      rule: 'minLength',
+      value: 10,
+      errorMessage: 'Dados Inválidos',
+    },
+    {
+      rule: 'maxLength',
+      value: 40,
+      errorMessage: 'Dados Inválidos',
+    },
+    {
+      rule: 'required',
+      errorMessage: 'Este campo é obrigatório!',
+    }
+  ])
+  .addField('#email', [
+    {
+      rule: 'required',
+      errorMessage: 'Este campo é obrigatório!',
+    },
+    {
+      rule: 'email',
+      errorMessage: 'Dados Inválidos',
+    },
+    {
+      rule: 'maxLength',
+      value: 50,
+      errorMessage: 'Dados Inválidos',
+    },
+  ])
+  .addField('#answer', [
+    {
+      rule: 'required',
+      errorMessage: 'Este campo é obrigatório!',
+    }
+  ])
+  .addField('#date', [
+    {
+      rule: 'required',
+      errorMessage: 'O campo de data é obrigatório.',
+    },
+  ]);
+
 function characterCounter() {
   let count = 0
   paraTextarea.innerText = count + '/500';
@@ -18,26 +80,17 @@ function characterCounter() {
 
 characterCounter();
 
-function checkEntry() {
-  let count = 0;
-  if (textareaAnswer.value.length < 10 || nameOfTheDev.value.length > nameOfTheDev.max || emailOfTheDev.value.length > emailOfTheDev.max) {
-    count += 1;
-  }
-  return count;
-}
-
 function sendButton() {
   for (let index = 0; index < btn.length; index += 1) {
     if (btn[index].innerText === 'Enviar') {
       btn[index].addEventListener('click', function(event) {
         if (inputCheckbox.checked !== true) {
           event.preventDefault();        
-        } else if (checkEntry() !== 0) {
-          alert('Dados Inválidos');
         } else {
           alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip')
           for (let index = 0; index < input.length; index += 1) {
             input[index].value = '';
+            input[index].checked = false;
           }
           textareaAnswer.value = '';
           characterCounter();
@@ -55,6 +108,8 @@ function clearButton() {
       btn[index].addEventListener('click', function() {
         for (let index = 0; index < input.length; index += 1) {
           input[index].value = '';
+          input[index].checked = false;
+
         }
         textareaAnswer.value = '';
         characterCounter();
