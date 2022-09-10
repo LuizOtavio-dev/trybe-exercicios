6dec2989e5631e09f2b8bc6c1e1eabdd146c81d1
LuizOtavio-dev <luiz.otavio273@gmail.com>
2022-09-09 22:55:12 -0300

    exercicio 1 - parte 3 - cria uma funcao para adicionar uma nova chave no objeto

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/script.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/script.js
@@ -1,5 +1,5 @@
 // Parte 2
-// Exercício 1
+// Exercício 1 e 2
 
 const order = {
   name: 'Rafael Andrade',
@@ -59,3 +59,30 @@ const orderModifier = (order) => {
 }
 
 orderModifier(order);
+
+// Parte 3
+// Exercício 1
+
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+const addKey = (obj, key, value) => obj[key] = value;
+
+addKey(lesson2, 'turno', 'noite');
