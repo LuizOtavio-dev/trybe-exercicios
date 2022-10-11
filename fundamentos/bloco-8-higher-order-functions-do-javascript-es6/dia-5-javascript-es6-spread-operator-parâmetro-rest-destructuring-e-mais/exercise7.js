c49248221288e6348952d10fc34304b0d03cda0c
LuizOtavio-dev <luiz.otavio273@gmail.com>
2022-10-10 23:18:25 -0300

    exercicio 7 - cria funcao que retorna o comprimento do navio

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parâmetro-rest-destructuring-e-mais/exercise7.js
@@ -0,0 +1,19 @@
+const ships = [
+  {
+    name: 'Titanic',
+    length: 269.1,
+    measurementUnit: 'meters',
+  },
+  {
+    name: 'Queen Mary 2',
+    length: 1132,
+    measurementUnit: 'feet',
+  },
+  {
+    name: 'Yamato',
+    length: 256,
+    measurementUnit: 'meters',
+  },
+];
+
+const shipLength = ({name, length, measurementUnit}) => `${name} is ${length} ${measurementUnit} long`;
