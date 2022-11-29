f73d2837d3de321065c62982e93472b18f485e0c
LuizOtavio-dev <luiz.otavio273@gmail.com>
2022-11-29 20:46:15 -0300

    consome a api para sortear um personagem ou exibe um alert caso o personagem nao seja encontrado

--- front-end/bloco-9-javascript-e-testes-assíncronos/dia-3-promises-e-fetch/src/main.js
+++ front-end/bloco-9-javascript-e-testes-assíncronos/dia-3-promises-e-fetch/src/main.js
@@ -1,1 +1,26 @@
 import './style.css';
+
+const imgSuperHero = document.querySelector('#image');
+const nameSuperHero = document.querySelector('#name');
+const btnSort = document.querySelector('#btn');
+
+const API = 'https://www.superheroapi.com/api.php/528883825782155';
+const MAX_HEROES = 1000;
+
+const randomId = () => Math.floor(Math.random() * MAX_HEROES);
+
+btnSort.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const id = randomId();
+
+  fetch(`${API}/${id}`)
+    .then((res) => res.json())
+    .then(({ image, name }) => {
+      imgSuperHero.src = image.url;
+      imgSuperHero.alt = `Imagem do super heroi ${name}`;
+      nameSuperHero.innerHTML = name;
+    })
+    // eslint-disable-next-line no-alert
+    .catch((error) => alert(error.message));
+});
