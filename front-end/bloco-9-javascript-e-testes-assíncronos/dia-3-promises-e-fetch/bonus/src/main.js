c56b196f9c8c4afc68467540e473ea0a4788add5
LuizOtavio-dev <luiz.otavio273@gmail.com>
2022-11-30 00:18:32 -0300

    exercicio 3 - bonus - faz uma requisicao para api que retorna uma imagem de um cachorro

--- front-end/bloco-9-javascript-e-testes-assíncronos/dia-3-promises-e-fetch/bonus/src/main.js
+++ front-end/bloco-9-javascript-e-testes-assíncronos/dia-3-promises-e-fetch/bonus/src/main.js
@@ -1,1 +1,18 @@
 import './style.css';
+
+const btnGetDog = document.querySelector('#btn-get-dog');
+// const btnGetCat = document.querySelector('#btn-get-cat');
+// const btnSuprise = document.querySelector('#btn-surprise');
+const imgRandom = document.querySelector('#img-random');
+
+const API = 'https://dog.ceo/api/breeds/image/random';
+
+btnGetDog.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  fetch(API)
+    .then((res) => res.json())
+    .then(({ message }) => {
+      imgRandom.src = message;
+    });
+});
