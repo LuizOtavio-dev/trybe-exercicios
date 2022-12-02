ffaabab973ac4f8b6c4fe42e81ccb5821cb03c50
LuizOtavio-dev <luiz.otavio273@gmail.com>
2022-12-01 23:31:21 -0300

    req 3 - faz iteracao com api e lista as moedas

--- front-end/bloco-9-javascript-e-testes-assíncronos/dia-4-pratica-casa-de-cambio/src/main.js
+++ front-end/bloco-9-javascript-e-testes-assíncronos/dia-4-pratica-casa-de-cambio/src/main.js
@@ -1,1 +1,27 @@
 import './style.css';
+
+const inputCoin = document.querySelector('#input-coin');
+const btnSearch = document.querySelector('#btn-search');
+const listCoin = document.querySelector('#list-coin');
+
+const API = 'https://api.exchangerate.host';
+
+const createList = (data) => {
+  const rates = Object.entries(data);
+
+  rates.forEach(rate => {
+    const li = document.createElement('li');
+    li.innerHTML = `${rate[0]}: ${rate[1]}`;
+    li.className = 'rate';
+    listCoin.appendChild(li);
+  });
+};
+
+btnSearch.addEventListener('click', () => {
+  const COIN = inputCoin.value;
+
+  fetch(`${API}/${COIN}`)
+    .then(res => res.json())
+    .then(({ rates }) => createList(rates))
+    .catch(error => window.alert(error.message));
+});
