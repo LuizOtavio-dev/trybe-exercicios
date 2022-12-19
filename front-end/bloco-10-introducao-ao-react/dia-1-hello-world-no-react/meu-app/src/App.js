d3f200ef66a944785865e082ed39a0e8a59eb1fb
LuizOtavio-dev <luiz.otavio273@gmail.com>
2022-12-19 20:51:09 -0300

    req 3 - cria um componente footer

--- front-end/bloco-10-introducao-ao-react/dia-1-hello-world-no-react/meu-app/src/App.js
+++ front-end/bloco-10-introducao-ao-react/dia-1-hello-world-no-react/meu-app/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Content from './Content';
+import Footer from './Footer';
 import Header from './Header';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Header />
       <Content />
+      <Footer />
     </div>
   );
 }
